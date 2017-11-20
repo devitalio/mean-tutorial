f9ada86782150748569b7c88efa983e96aaba69a
devitalio <devitalio@users.noreply.github.com>
2017-11-20 21:11:05 +0200

    Initial commit

---
+++ app_server/controllers/locations.js
@@ -0,0 +1,11 @@
+module.exports.homeList = function(req, res, next) {
+  res.render('locations-list', { title: 'Home' });
+};
+
+module.exports.locationInfo = function(req, res, next) {
+  res.render('location-info', { title: 'Location Info' });
+};
+
+module.exports.addReview = function(req, res, next) {
+  res.render('location-review-form', { title: 'Add Review' });
+};
---
+++ app_server/routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+var locationsController = require('../controllers/locations');
+var otherController = require('../controllers/other');
+
+/* GET various location paged */
+router.get('/', locationsController.homeList);
+router.get('/location', locationsController.locationInfo);
+router.get('/location/review/new', locationsController.addReview);
+
+router.get('/about', otherController.about);
+
+module.exports = router;
