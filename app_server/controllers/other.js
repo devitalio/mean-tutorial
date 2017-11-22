5d77953c673619868eb33547b89cb163aba65929
devitalio <devitalio@users.noreply.github.com>
2017-11-22 22:31:24 +0200

    refactor about page

--- app_server/controllers/other.js
+++ app_server/controllers/other.js
@@ -1,3 +1,8 @@
 module.exports.about = function(req, res, next) {
-  res.render('generic-text', { title: 'About' });
+  res.render('generic-text', 
+  {
+    title: 'About',
+    content: 'Loc8r was created to help people find places to sit down and get a bit of work done.\n\n' +
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed lorem ac nisi dignissim accumsan.'
+  });
 };
