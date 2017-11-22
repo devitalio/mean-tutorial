d482d031f6eecc21a819e3a2539878ac90733b44
devitalio <devitalio@users.noreply.github.com>
2017-11-22 21:03:12 +0200

    refactor to use loops

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -2,7 +2,31 @@ module.exports.homeList = function(req, res, next) {
   res.render('locations-list',
   { 
     title: 'Loc8r - find a place to work with wifi',
-    pageHeader: { title: 'Loc8r', strapline: 'Find places to work with wifi near you!'}
+    pageHeader: { title: 'Loc8r', strapline: 'Find places to work with wifi near you!'},
+    locations: 
+    [
+      {
+        name: 'Starcups',
+        address: 'Decebal 12',
+        rating: 5,
+        facilities: ['Hot drinks', 'Premium WiFi'],
+        distance: '100m'
+      },
+      {
+        name: 'Coffee Queens',
+        address: 'Memorandumului 5',
+        rating: 4,
+        facilities: ['Food', 'Wifi'],
+        distance: '500m'
+      },
+      {
+        name: 'Burger King',
+        address: 'Tipografiei 15',
+        rating: 3,
+        facilities: ['Food', 'Hot Drinks', 'Premium Wifi'],
+        distance: '300m'
+      }
+    ] 
   });
 };
 
