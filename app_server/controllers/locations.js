90da3c8b712d04d39aa2a89b8dca6b5af6b75732
devitalio <devitalio@users.noreply.github.com>
2017-11-22 22:16:48 +0200

    refactor detail page

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -3,11 +3,12 @@ module.exports.homeList = function(req, res, next) {
   { 
     title: 'Loc8r - find a place to work with wifi',
     pageHeader: { title: 'Loc8r', strapline: 'Find places to work with wifi near you!'},
+    sidebar: 'Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you\'re looking for.',
     locations: 
     [
       {
-        name: 'Starcups',
-        address: 'Decebal 12',
+        name: 'La Rochelle',
+        address: 'str. Traian 2',
         rating: 5,
         facilities: ['Hot drinks', 'Premium WiFi'],
         distance: '100m'
@@ -31,7 +32,36 @@ module.exports.homeList = function(req, res, next) {
 };
 
 module.exports.locationInfo = function(req, res, next) {
-  res.render('location-info', { title: 'Location Info' });
+  res.render('location-info', 
+  { 
+    title: 'La Rochelle',
+    pageHeader: {title: 'La Rochelle'},
+    sidebar:
+    {
+      context:'is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.',
+      callForAction: 'If you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you.'
+    },
+    location:
+    {
+      name: 'La Rochelle',
+      address: 'str. Traian 2',
+      rating: 5,
+      facilities: ['Hot drinks', 'Premium WiFi'],
+      distance: '100m',
+      coords: {lat: 46.776446, lng: 23.590588},
+      openningTimes:
+      [
+        { days: 'Mon - Fri', opening: '07:00am', closing: '07:00pm', closed: false},
+        { days: 'Sat', opening: '08:00am', closing: '05:00pm', closed: false},
+        { days: 'Sun', time: '', closed:true}
+      ],
+      reviews:
+      [
+        {author: 'Steve Jobs', rating:5, timestamp:'16 July 2017', reviewText:'What a great place. I can\'t say enough good things \n about it.'},
+        {author: 'Bill Gates', rating:5, timestamp:'20 Sep 2017', reviewText:'It was okay. Coffee wasn\'t great, but the wifi was fast.'}
+      ]
+    }
+  });
 };
 
 module.exports.addReview = function(req, res, next) {
