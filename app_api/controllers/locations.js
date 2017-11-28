c1aa22dbdcb47988413b9e4af2b7cda522df8acd
devitalio <devitalio@users.noreply.github.com>
2017-11-28 21:20:26 +0200

    add some controllers for REST API

---
+++ app_api/controllers/locations.js
@@ -0,0 +1,17 @@
+helper = require('../lib/helpers');
+
+module.exports.locationsListByDistance = function(req, res){
+}
+
+module.exports.locationsCreate = function(req, res){
+  helpers.sendJSONResponce(res, 200, {'status':'success'});
+}
+
+module.exports.locationsReadOne = function(req, res){
+}
+
+module.exports.locationsDeleteOne = function(req, res){
+}
+
+module.exports.locationsUpdateOne = function(req, res){
+}
---
+++ app_api/controllers/reviews.js
@@ -0,0 +1,11 @@
+module.exports.reviewsCreateOne = function(req, res){
+}
+
+module.exports.reviewsReadOne = function(req, res){
+}
+
+module.exports.reviewsUpdateOne = function(req, res){
+}
+
+module.exports.reviewsDeleteOne = function(req, res){
+}
