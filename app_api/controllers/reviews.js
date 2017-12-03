0a342e35db44b4f72b4224d24ba44c42aad25aec
devitalio <devitalio@users.noreply.github.com>
2017-12-03 18:04:56 +0200

    added ids to reviews, added controller for querying reviws by id

--- app_api/controllers/reviews.js
+++ app_api/controllers/reviews.js
@@ -1,7 +1,49 @@
+var helpers = require('../lib/helpers');
+var mongoose = require('mongoose');
+
+var loc = mongoose.model('location');
+
 module.exports.reviewsCreateOne = function(req, res){
 }
 
 module.exports.reviewsReadOne = function(req, res){
+  if(!req.params || !req.params.locationid || !req.params.reviewid){
+    helpers.sendJsonResponse(res, 404, {message: "No location or review id in request"});
+    return;
+  }
+    
+  loc.findById(req.params.locationid)
+     .select('name reviews')
+     .exec(function(err, location)
+      {
+        var response, review;
+        
+        if(!location){
+          helpers.sendJsonResponse(res, 404, {message: "No location found"});
+          return;
+        }
+        else if(err){
+          helpers.sendJsonResponse(res, 404, {message: "ERROR" + err});
+          return;
+        }      
+        
+        if (location.reviews && location.reviews.length > 0) {
+          review = location.reviews.id(req.params.reviewid);
+          if (!review) {
+            helpers.sendJsonResponse(res, 404, { "message": "reviewid not found"});
+          }
+          else {
+            response = { location : { name : location.name, id : req.params.locationid},
+                         review : review };
+            helpers.sendJsonResponse(res, 200, response);
+          }
+        }
+        else {
+          helpers.sendJsonResponse(res, 404, { "message": "No reviews found"});
+        }
+        
+        helpers.sendJsonResponse(res, 200, location);
+      });
 }
 
 module.exports.reviewsUpdateOne = function(req, res){
--- app_api/models/insertScript.js
+++ app_api/models/insertScript.js
@@ -1,6 +1,25 @@
 conn = new Mongo();
 db = conn.getDB('loc8r');
 
+db.locations.insertOne(
+  {
+    name : "La Rochelle",
+    address : "str. Traian 2",
+    rating : 5,
+    facilities : [ "Hot drinks", "Premium WiFi"],
+    coords : [ 46.776446, 23.590588 ],
+    openingTimes :
+    [
+        { days : "Mon - Fri", opening : "07:00am", closing : "07:00pm", closed : false },
+        { days : "Sat", opening : "08:00am", closing : "05:00pm", closed : false},
+        { days : "Sun", time : "", closed : true}
+    ],
+    reviews :
+    [
+        { _id: ObjectId(), author : "Steve Jobs", rating : 5, timestamp : ISODate("2017-07-15T21:00:00Z"), reviewText : "What a great place. I can't say enough good things \n about it." }
+    ]
+});
+
 db.locations.insertOne({
       name: 'KFC',
       address: 'Strada Iuliu Maniu 1',
@@ -15,7 +34,7 @@ db.locations.insertOne({
       ],
       reviews:
       [
-        {author: 'Steve Jobs', rating:5, timestamp:new Date('15 Nov 2017'), reviewText:'What a great place. I can\'t say enough good things \n about it.'},
-        {author: 'Bill Gates', rating:5, timestamp:new Date('1 Oct 2017'), reviewText:'It was okay. Coffee wasn\'t great, but the wifi was fast.'}
+        { _id: ObjectId(), author: 'Steve Jobs', rating:5, timestamp:new Date('15 Nov 2017'), reviewText:'What a great place. I can\'t say enough good things \n about it.'},
+        { _id: ObjectId(), author: 'Bill Gates', rating:5, timestamp:new Date('1 Oct 2017'), reviewText:'It was okay. Coffee wasn\'t great, but the wifi was fast.'}
       ]
 });
