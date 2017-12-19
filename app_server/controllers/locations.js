e524932aac1a5b42d7ce8598d9c9eaf7226b6db7
devitalio <devitalio@users.noreply.github.com>
2017-12-19 21:38:52 +0200

    date formatting

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -82,45 +82,28 @@ module.exports.locationInfo = function(req, res) {
   
   request(requestOptions, function(err, response, body) {
     
-    console.log(response.statusCode);
+    var data = body;
+    data.coords = { lat: body.coords[1], lng: body.coords[0]};
+    console.log(data);
     if(response.statusCode === 200){
-      renderDetailPage(req, res);
+      renderDetailPage(req, res, data);
     }   
   });
   
 
 };
 
-var renderDetailPage = function(req, res) {
+var renderDetailPage = function(req, res, data) {
   res.render('location-info', 
   { 
-    title: 'La Rochelle',
-    pageHeader: {title: 'La Rochelle'},
+    title: data.name,
+    pageHeader: {title: data.name},
     sidebar:
     {
       context:'is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.',
       callForAction: 'If you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you.'
     },
-    location:
-    {
-      name: 'La Rochelle',
-      address: 'str. Traian 2',
-      rating: 5,
-      facilities: ['Hot drinks', 'Premium WiFi'],
-      distance: '100m',
-      coords: {lat: 46.776446, lng: 23.590588},
-      openingTimes:
-      [
-        { days: 'Mon - Fri', opening: '07:00am', closing: '07:00pm', closed: false},
-        { days: 'Sat', opening: '08:00am', closing: '05:00pm', closed: false},
-        { days: 'Sun', time: '', closed:true}
-      ],
-      reviews:
-      [
-        {author: 'Steve Jobs', rating:5, timestamp:'16 July 2017', reviewText:'What a great place. I can\'t say enough good things \n about it.'},
-        {author: 'Bill Gates', rating:5, timestamp:'20 Sep 2017', reviewText:'It was okay. Coffee wasn\'t great, but the wifi was fast.'}
-      ]
-    }
+    location: data
   });
 };
 
