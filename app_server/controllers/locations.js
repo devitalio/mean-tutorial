7b057739c23c187fdb739730ac82d45afe63af85
devitalio <devitalio@users.noreply.github.com>
2017-12-21 20:47:25 +0200

    error handling app side (no DB)

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -150,15 +150,19 @@ module.exports.doAddReview = function(req, res) {
     json : postdata
   };
   
-  request(requestOptions, function(err, response, body) {
-    if(response.statusCode === 201){
-     res.redirect('/location/' + locationid);
-    }
-    else if(response.statusCode === 400 && body.name && body.name === "ValidationError"){
-      res.redirect('/location/'+ locationid+ '/review/new?err=val' )
-    }
-    else {
-      _showError(req, res, response);
-    }
-  });
+  if (!postdata.author || !postdata.rating || !postdata.reviewText) {
+    res.redirect('/location/' + locationid + '/reviews/new?err=val');
+  } else {
+    request(requestOptions, function(err, response, body) {
+      if(response.statusCode === 201){
+        res.redirect('/location/' + locationid);
+      }
+      else if(response.statusCode === 400 && body.name && body.name === "ValidationError"){
+        res.redirect('/location/'+ locationid+ '/review/new?err=val' )
+      }
+      else {
+        _showError(req, res, response);
+      }
+    });
+  }
 }
